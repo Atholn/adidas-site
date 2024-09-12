5f5d89db21a18f8814d6f1b8655d9cfa23f3f78c
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-12 22:32:07 +0200

    #1 add first logic header

--- src/components/basic-components/header/header.tsx
+++ src/components/basic-components/header/header.tsx
@@ -1,8 +1,29 @@
+import { render } from "@testing-library/react";
 import "./header.scss";
+import { useState } from "react";
 
 function Header() {
-  return <div className="header">
+  const [show, toggleShow] = useState(true);
+  let xD:boolean = false;
 
+  const handleUrlSite = () => {
+    toggleShow(!show)
+  };
+
+  return <div className="header">
+    <div>
+      {show && 
+        <a onClick={handleUrlSite}>
+          DARMOWE ZWROTY I DOSTAWA
+        </a>
+      } 
+
+      {!show && 
+        <a onClick={handleUrlSite}>
+          DARMOWE ZWROTY I DOSTAWA2222222222222
+        </a>
+      }
+    </div>
   </div>
 }
 
